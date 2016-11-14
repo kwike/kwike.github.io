2c0a3280512f3d5981f65f19e47876b91b642f11
Abhay Saxena <kwike.entcorp@gmail.com>
2016-11-14 20:24:55 +0530

    Update lib.js

--- lib.js
+++ lib.js
@@ -21,9 +21,13 @@ document.documentElement.body.src = get('s');
 /*for share button*/
 function shareopen() {
     document.getElementById("share").style.width = "230px";
+    document.getElementById("closeshare").style.display = "block";
+	document.getElementById("sharebtn").style.display = "none";
 }
 function shareclose() {
     document.getElementById("share").style.width = "0px";
+	 document.getElementById("closeshare").style.display = "none";
+	document.getElementById("sharebtn").style.display = "block";
 }
 /*w3schools library*/
 var w3DataObject = {};
